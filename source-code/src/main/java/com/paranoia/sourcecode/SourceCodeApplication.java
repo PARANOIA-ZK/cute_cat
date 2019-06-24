b473541b0eccab3819e8ecc4d295798d5d178c03
designinn <paranoia_zk@yeah.net>
2019-06-24 22:16:41 +0800

    source code : ThreadLocal

---
+++ source-code/src/main/java/com/paranoia/sourcecode/SourceCodeApplication.java
@@ -0,0 +1,13 @@
+package com.paranoia.sourcecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceCodeApplication.class, args);
+    }
+
+}
