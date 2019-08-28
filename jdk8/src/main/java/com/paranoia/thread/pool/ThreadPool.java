1d6349a3c07746a83293f820ce0da95e58f5e644
张凯 <paranoia_zk@yeah.net>
2019-08-28 15:12:24 +0800

    save

---
+++ jdk8/src/main/java/com/paranoia/thread/pool/ThreadPool.java
@@ -0,0 +1,36 @@
+package com.paranoia.thread.pool;
+
+/**
+ * @author ZHANGKAI
+ * @date 2019/8/28
+ * @description
+ */
+public interface ThreadPool<Job extends Runnable> {
+    /**
+     * 执行一个Job，这个Job需要实现Runnable
+     */
+    void execute(Job job);
+
+    /**
+     * 关闭线程池
+     */
+    void shutdown();
+
+    /**
+     * 增加工作者线程
+     * @param num
+     */
+    void addWorkers(int num);
+
+    /**
+     * 减少工作者线程
+     * @param num
+     */
+    void removeWorker(int num);
+
+    /**
+     * 得到正在等待执行的任务数量
+     * @return
+     */
+    int getJobSize();
+}
